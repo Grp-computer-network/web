5ff741ca562c2cd90275d406e864ba07d893c579
Grp-computer-network <krithickprofessional@gmail.com>
2024-12-20 09:20:20 +0530

    navbar done

---
+++ Adminstration.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Adminstration</title>
+    <?php 
+    include('reuseable_files/style-php.html');
+    ?>
+   <link rel="stylesheet" href="css/tailwind.css">
+</head>
+<body>
+<?php
+include("reuseable_files/header.html");
+?>    
+  <div class="title-iqac">
+        <h1 style='font-size:23px; font-weight:bold;'>Adminstration</h1>
+    </div>
+    <!-- IQAC Section -->
+    <section id="iqac-container">
+        <!-- Left-side IQAC Navigation -->
+        <nav id="iqac-nav">
+            <ul>
+                <li><a href="/COLLEGE_WEB/Administration//leadership.html" class="iqac-link">Leadership</a></li>
+                <li><a href="/COLLEGE_WEB/Administration//committee-msg.html" class="iqac-link">Committee Members</a></li>
+                <li><a href="/COLLEGE_WEB/Administration//Academic-Leadership.html" class="iqac-link">Academics Leadership</a></li>
+                <!-- More Sub-Navigation Links -->
+            </ul>
+        </nav>
+        <!-- Main Content Section where the IQAC content will be loaded -->
+        <div id="iqac-content">
+            <h2>Welcome to IQAC</h2>   <br>
+            <p style="font-size: 20px;padding: 10px;">IQAC would be looked upon as the prime quality sustenance measure.
+
+                As providing quality education, adopting innovative teaching-learning methods, and assessing them is a continuous process, IQAC will be working with the HEI to support them throughout their teaching-learning journey.
+                
+                Let’s understand the concept of IQAC & how it can help institutions.</p>
+        </div>
+        </section>
+  
+<?php 
+  include("reuseable_files/footer.html");
+  ?>     
+  <?php 
+       include('addmission_form/admission_form.html');
+        ?>
+    <script src="script/addmission.js"></script>
+    <script src="script/iqac.js"></script>
+<script src="script/fixed-nav.js"></script>  
+</body>
+</html>
